115836bd53d13021e3a7145c0dd9fdd624eb79bb
HarpSeal1234 <blueguh2010@gmail.com>
2025-11-23 16:50:23 -0500

    updates to test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/24-25/basic.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Disabled
+@Autonomous(name="basic", group="Robot")
+public class basic extends LinearOpMode {
+
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
