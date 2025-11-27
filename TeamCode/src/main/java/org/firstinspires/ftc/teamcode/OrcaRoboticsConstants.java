fecf013b4934dae0ecbd403606d708871e1c0553
HarpSeal1234 <blueguh2010@gmail.com>
2025-11-27 14:48:21 -0500

    updated constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OrcaRoboticsConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OrcaRoboticsConstants.java
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
 public class OrcaRoboticsConstants {
+  // OUTTAKE
   public final static double FAR_OUTTAKE_VELOCITY = 1700;
   public final static double CLOSE_OUTTAKE_VELOCITY = 1250;
 
+  // DRIVE POWER
+  public final static double DRIVE_POWER = 0.7;
+
   // INTAKE
   public final static double PUSH_POWER = 0.9;
-  public final static double INTAKE_POWER = 1.0;
+  public final static double INTAKE_POWER = 0.8;
+  public final static double INTAKE_ZERO_POWER = 0.8;
+
 
   // LEFT BLOCKER
   public final static double L_BLOCKER_UP = 0.03;
@@ -16,5 +22,8 @@ public class OrcaRoboticsConstants {
   public final static double R_BLOCKER_UP = 0.65;
   public final static double R_BLOCKER_DOWN = 0.0;
 
+  // PIVOT
+  public final static double FAR_PIVOT_POSITION = 0.7;
+  public final static double CLOSE_PIVOT_POSITION = 0.3;
 
 }
